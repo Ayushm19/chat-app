f9e3a8072ee4499cce245bb0e8c66a018a98bcc1
Ayush Mishra <knandan400@gmail.com>
2024-09-05 09:40:18 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-3efn.onrender.com/login", {
+			const socket = io("https://chat-app-3efn.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
